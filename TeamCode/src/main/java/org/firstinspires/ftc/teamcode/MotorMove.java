21de549c88d0f4a52d914bea97684461df182b3d
dorinon <dor.inon01@gmail.com>
2022-06-02 20:44:20 +0300

    field centric not working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorMove.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorMove.java
@@ -1,33 +1,28 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.teamcode.recourses14872.HardwarePush;
 
-@Autonomous(name = "motor1")
+@TeleOp(name = "MecanumMove")
 public class MotorMove extends OpMode {
-
+    HardwarePush map = new HardwarePush(this);
 
     @Override
     public void init() {
+        map.init();
 
-        HardwarePush map = new HardwarePush();
-        map.init(hardwareMap);
     }
+
 
     @Override
     public void loop() {
-
-        intake.setPower(gamepad1.right_trigger);
-        intake.setPower(-gamepad1.left_trigger);
-        if (gamepad1.right_bumper) {
-            arm.setPower(-0.5);
-        } else {
-            arm.setPower(-0.01);
-        }
+        Orientation angles = map.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        map.mecanum();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/recourses14872/HardwarePush.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/recourses14872/HardwarePush.java
@@ -1,25 +1,87 @@
 package org.firstinspires.ftc.teamcode.recourses14872;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 public class HardwarePush {
 
-    public DcMotor leftDrive;
-    public DcMotor rightDrive;
-    public DcMotor intake;
-    public DcMotor arm;
+    private DcMotor lf;
+    private DcMotor rf;
+    private DcMotor lb;
+    private DcMotor rb;
+    private final OpMode opMode;
+    public BNO055IMU imu;
 
-    public void init(HardwareMap map){
+    public  HardwarePush(final OpMode opMode){
+        this.opMode = opMode;
+    }
 
-        leftDrive = map.get(DcMotor.class, "");
-        rightDrive = map.get(DcMotor.class, "");
-        intake = map.get(DcMotor.class, "intake");
-        arm = map.get(DcMotor.class, "arm");
+    public void init(){
+        lf = opMode.hardwareMap.get(DcMotor.class, "lf");
+        rf = opMode.hardwareMap.get(DcMotor.class, "rf");
+        lb = opMode.hardwareMap.get(DcMotor.class, "lb");
+        rb = opMode.hardwareMap.get(DcMotor.class, "rb");
+        lf.setDirection(DcMotorSimple.Direction.REVERSE);
+        lb.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        leftDrive.setPower(0);
-        rightDrive.setPower(0);
-        intake.setPower(0);
-        arm.setPower(0);
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.RADIANS;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = opMode.hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+    }
+
+    public void mecanum() {
+
+        final Vector joystickVector = new Vector(gamepad1.left_stick_x, gamepad1.left_stick_y);
+        final float robotAngle = (float) Math.toRadians(-getAngle());
+        final Vector rotated = joystickVector.rotate(robotAngle);
+        drive(rotated.y, rotated.x, gamepad1.right_trigger - gamepad1.left_trigger);
+    }
+
+    public void drive(float y, float x, float r) {
+        final float lfPower = y + x + r;
+        final float rfPower = y - x + r;
+        final float lbPower = y - x - r;
+        final float rbPower = y + x - r;
+
+        lf.setPower(lfPower);
+        rf.setPower(rfPower);
+        rb.setPower(rbPower);
+        lb.setPower(lbPower);
+    }
+
+
+    public float getAngle(){
+        return wrap(imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle);
+    }
+
+    public float wrap(float angle){
+        final float wrapped = angle % 360;
+        if (wrapped > 180) {
+            return wrapped - 360;
+        } else if (wrapped < -180) {
+            return wrapped + 360;
+        } else {
+            return wrapped;
+        }
     }
 }
