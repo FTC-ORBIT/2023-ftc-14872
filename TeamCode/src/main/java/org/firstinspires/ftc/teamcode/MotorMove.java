0709e699c884038dfa9e7e1f8a9cef4588eea2e0
dorinon <dor.inon01@gmail.com>
2022-06-12 14:43:39 +0300

    Added gyro value to dashboard graph

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorMove.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorMove.java
@@ -1,8 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
@@ -12,17 +16,19 @@ import org.firstinspires.ftc.teamcode.recourses14872.HardwarePush;
 @TeleOp(name = "MecanumMove")
 public class MotorMove extends OpMode {
     HardwarePush map = new HardwarePush(this);
-
     @Override
     public void init() {
         map.init();
-
     }
 
 
     @Override
     public void loop() {
-        Orientation angles = map.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
         map.mecanum();
+        telemetry.addData("Gyro",map.getAngle() * (180/Math.PI));
+        TelemetryPacket packet = new TelemetryPacket();
+        packet.put("Gyro",map.getAngle() * (180/Math.PI));
+        FtcDashboard.getInstance().sendTelemetryPacket(packet);
     }
 }
+
