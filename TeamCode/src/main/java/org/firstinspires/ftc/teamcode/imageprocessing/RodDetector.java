5b857905e29b66594b95a55f26da54a4ce0fb0ea
AlmogSha1 <a0522823767@gmail.com>
2022-09-16 00:09:05 +0300

    added contours not done yet (I put it as a comment for now)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/imageprocessing/RodDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/imageprocessing/RodDetector.java
@@ -1,15 +1,20 @@
 package org.firstinspires.ftc.teamcode.imageprocessing;
 
 import android.location.Location;
+import android.media.Image;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RodDetector extends OpenCvPipeline {
     //creates a telemetry
@@ -38,7 +43,9 @@ public class RodDetector extends OpenCvPipeline {
     public Mat processFrame(Mat input) {
         //set the filter and filter color censoring
         Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
+        //low color setting
         Scalar lowHSV = new Scalar(0 , 55 , 100);
+        //high color setting
         Scalar highHSV = new Scalar(32, 255, 255);
         Core.inRange(mat, lowHSV, highHSV, mat);
 
@@ -90,3 +97,28 @@ public class RodDetector extends OpenCvPipeline {
         return RodIn;
     }
 }
+/*
+class contours extends OpenCvPipeline {
+
+    @Override
+    public Mat processFrame(Mat input) {
+        Mat mat = new Mat();
+        //turning image to binary image with the right settings
+        //binary = black & white image (the object is white)
+        //in this code we are turning the color yellow to white and else to black
+        Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
+        Scalar lowHSV = new Scalar(0 , 55 , 100);
+        Scalar highHSV = new Scalar(32, 255, 255);
+        Core.inRange(mat, lowHSV, highHSV, mat);
+        //finding the contours
+        List<MatOfPoint> contours = new ArrayList<>();
+        //draw the contour
+        Mat hierarchey = new Mat();
+        Imgproc.findContours(input, contours, hierarchey , Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE );
+        Scalar color = new Scalar(0, 0, 255);
+        Imgproc.drawContours(input, contours, -1, color, 2, Imgproc.LINE_8, hierarchey, 2, new Point() ) ;
+
+        return null;
+    }
+}
+*/
