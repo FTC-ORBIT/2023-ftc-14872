5aaf01f5dc2270b686a3c2355c62ae03beb4073f
Birdoflames <89009999+dorinon@users.noreply.github.com>
2022-11-15 15:43:05 +0200

    added servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp14872.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp14872.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.res.Gyro;
@@ -19,6 +21,7 @@ public class TeleOp14872 extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         Gyro.init(hardwareMap);
         Drivetrain.init(hardwareMap);
+        Servo servo = hardwareMap.get(Servo.class, "servoArm");
 
         GlobalData.isAutonomous = false;
 
@@ -27,6 +30,8 @@ public class TeleOp14872 extends LinearOpMode {
         while (!isStopRequested()){
             GlobalData.currentTime = timer.milliseconds();
             Drivetrain.operate(new Vector(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.left_trigger - gamepad1.right_trigger));
+            if (gamepad1.dpad_up) {servo.setPosition(90);}
+            if (gamepad1.dpad_down) {servo.setPosition(0);}
 
             GlobalData.deltaTime = GlobalData.currentTime - GlobalData.lastTime;
 
