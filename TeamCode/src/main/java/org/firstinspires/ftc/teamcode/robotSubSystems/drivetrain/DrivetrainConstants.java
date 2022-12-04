04e8b604c53f26892920cd0eb4674f110ad50a7d
IdoDePro <idozipori08@gmail.com>
2022-12-04 14:14:51 +0200

    sdk update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/drivetrain/DrivetrainConstants.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.robotSubSystems.drivetrain;
-
-public class DrivetrainConstants {
-    //TODO: find values
-    public static final float trackWidth = 0;
-    public final static float forward_offset = 0;
-}
