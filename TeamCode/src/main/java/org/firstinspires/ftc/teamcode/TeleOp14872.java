35a07e71f65d489f2b17253921cca93159083f8c
Birdoflames <drnmalka@gmail.com>
2022-12-15 17:07:23 +0200

    added arm and some other stuff (working)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp14872.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp14872.java
@@ -1,10 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.robotSubSystems.elevator.Elevator;
 import org.firstinspires.ftc.teamcode.sensors.Gyro;
 import org.firstinspires.ftc.teamcode.robotData.GlobalData;
 import org.firstinspires.ftc.teamcode.robotSubSystems.drivetrain.Drivetrain;
@@ -18,19 +23,31 @@ public class TeleOp14872 extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         Gyro.init(hardwareMap);
-        Drivetrain.init(hardwareMap);
-
+        //Drivetrain.init(hardwareMap);
+        Elevator.init(hardwareMap);
+        int level = 0;
         GlobalData.isAutonomous = false;
-
+        Servo servo = hardwareMap.get(Servo.class, "servo");
         waitForStart();
 
         while (!isStopRequested()){
             GlobalData.currentTime = timer.milliseconds();
-            Drivetrain.operate(new Vector(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.left_trigger - gamepad1.right_trigger));
+            //Drivetrain.operate(new Vector(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.left_trigger - gamepad1.right_trigger));
 
+            if (gamepad1.right_bumper) {servo.setPosition(90);}
+            if (gamepad1.left_bumper) {servo.setPosition(0);}
+
+            if (gamepad1.dpad_right) {level =1;}
+            if (gamepad1.dpad_down) {level =2;}
+            if (gamepad1.dpad_left) {level =3;}
+            Elevator.operate(gamepad1.dpad_left,gamepad1.dpad_down,gamepad1.dpad_right,level);
+            if (gamepad1.a) {Elevator.elevatorMotor.setPower(0.4);}
+            if (gamepad1.b) {Elevator.elevatorMotor.setPower(0);}
             GlobalData.deltaTime = GlobalData.currentTime - GlobalData.lastTime;
 
             GlobalData.lastTime = GlobalData.currentTime;
+            if (gamepad1.dpad_down) {telemetry.addData("somthing:", "333333333333333333333333333");}
+            if (gamepad1.dpad_left) {telemetry.addData("jkbvkwbekvj", "efweivfewvfef");}
             telemetry.update();
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/elevator/Elevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotSubSystems/elevator/Elevator.java
@@ -1,42 +1,76 @@
 package org.firstinspires.ftc.teamcode.robotSubSystems.elevator;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+
+import android.widget.Switch;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 
 public class Elevator {
-    private static DcMotor elevatorMotor;
+    public static DcMotor elevatorMotor;
 
-    public void init(HardwareMap hardwareMap){
+    public static void init(HardwareMap hardwareMap){
         elevatorMotor = hardwareMap.get(DcMotor.class, "elevatorMotor");
         elevatorMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        elevatorMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        elevatorMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        elevatorMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
-    public void operate(int level){
+    public static void operate(boolean level1, boolean level2, boolean level3,int level) {
         if(level == 1) {
-            //in ticks
-            elevatorMotor.setTargetPosition(1645);
+            //in ticks 1645
+            elevatorMotor.setTargetPosition(1750);
+            elevatorMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         }
         else if(level == 2) {
             //in ticks
             elevatorMotor.setTargetPosition(2865);
+            elevatorMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         }
         else if(level == 3) {
             //in ticks
             elevatorMotor.setTargetPosition(4085);
+            elevatorMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         }
         else{
             elevatorMotor.setTargetPosition(0);
+            elevatorMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         }
+
+        /*
+        switch (level) {
+            case 1:
+                //in ticks
+                elevatorMotor.setTargetPosition(1654);
+                elevatorMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                break;
+            case 2:
+                //in ticks
+                elevatorMotor.setTargetPosition(2865);
+                elevatorMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                break;
+            case 3:
+                elevatorMotor.setTargetPosition(4085);
+                elevatorMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                break;
+            //default:
+                //elevatorMotor.setTargetPosition(0);
+        }
+
+         */
+        elevatorMotor.setPower(0.4);
     }
 
-    public void stop(){
+    public static void stop(){
         elevatorMotor.setPower(0);
     }
 
 
-    public int getPosition() {
+    public static int getPosition() {
         return elevatorMotor.getCurrentPosition();
     }
 }
